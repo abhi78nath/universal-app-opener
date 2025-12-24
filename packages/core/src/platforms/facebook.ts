bea39449ea88f2128e7eb225eaedd6a8342be751
Badshass4 <badsha1695@gmail.com>
2025-12-24 16:30:30 +0530

    modified match regex to support mobile web version of facebook

--- packages/core/src/platforms/facebook.ts
+++ packages/core/src/platforms/facebook.ts
@@ -1,15 +1,15 @@
 import { DeepLinkHandler } from '../types';
 
 export const facebookHandler: DeepLinkHandler = {
-  match: (url) => url.match(/(?:www\.)?facebook\.com\/([^#]+)/),
+  match: (url) => url.match(/^https?:\/\/(?:www\.|m\.)?facebook\.com\/(.*)$/),
 
-  build: (webUrl) => {
-    const encodedUrl = webUrl.replace(/^https?:\/\//, '');
+  build: (webUrl, match) => {
+    const url = `facebook.com/${match[1]}`;
 
     return {
       webUrl,
-      ios: `fb://facewebmodal/f?href=${encodedUrl}`,
-      android: `intent://${encodedUrl}#Intent;scheme=https;package=com.facebook.katana;end`,
+      ios: `fb://facewebmodal/f?href=${url}`,
+      android: `intent://${url}#Intent;scheme=https;package=com.facebook.katana;end`,
       platform: 'facebook',
     };
   },

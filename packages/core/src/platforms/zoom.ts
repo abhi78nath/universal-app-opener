ababdb087381e8dce21c93079b73c57aba762911
abhijithwinddaa <abhijithyadav786@gmail.com>
2026-01-03 11:46:47 +0530

    feat: add support for /s/ paths (scheduled meetings) in Zoom handler

--- packages/core/src/platforms/zoom.ts
+++ packages/core/src/platforms/zoom.ts
@@ -5,6 +5,7 @@ import { DeepLinkHandler } from '../types';
  *
  * Supports:
  * - Meeting links: https://zoom.us/j/1234567890
+ * - Scheduled meeting links: https://zoom.us/s/1234567890
  * - Meeting links with password: https://zoom.us/j/1234567890?pwd=abcdef
  * - Meeting links with password in any query position: https://zoom.us/j/1234567890?uname=Guest&pwd=abcdef
  * - Meeting links from subdomains: https://us02web.zoom.us/j/1234567890
@@ -15,9 +16,10 @@ import { DeepLinkHandler } from '../types';
  */
 export const zoomHandler: DeepLinkHandler = {
   match: (url) => {
-    // Match zoom.us/j/<meeting_id> or *.zoom.us/j/<meeting_id>
+    // Match zoom.us/[js]/<meeting_id> or *.zoom.us/[js]/<meeting_id>
     // Only capture the meeting ID here, password is extracted separately
-    return url.match(/^https?:\/\/(?:[a-z0-9-]+\.)?zoom\.us\/j\/(\d+)/i);
+    // Added 'i' flag for case-insensitive subdomain matching
+    return url.match(/^https?:\/\/(?:[a-z0-9-]+\.)?zoom\.us\/[js]\/(\d+)/i);
   },
 
   build: (webUrl, match) => {
